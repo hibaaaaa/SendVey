a3c2378066ec8bc61f80c9ca8195fd6f55ccdfd3
Hiba Bagané <bagane.hiba@polymtl.ca>
2017-08-10 16:13:33 -0400

    server setup

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+
+//route handler watching for incoming HTTP requests
+app.get("/", (req, res) => {
+  res.send({ hi: "there" });
+});
+//look at the environment and see if Heroku has
+//declared a port for us to use
+//if there is an environment variable already defined
+//by Heroku go ahead otherwise just use port 5000
+const PORT = process.env.PORT || 5000;
+app.listen(PORT);
