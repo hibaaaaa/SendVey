f68f5f8a56cc93ad7de2c646ea1b48a36421c69f
Hiba Bagané <bagane.hiba@polymtl.ca>
2017-08-14 12:32:07 -0400

    add user model class

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const mongoose = require('mongoose');
-require('./services/passport');
 const keys = require('./config/keys');
+require('./services/passport');
+require('./models/user');
 
 mongoose.connect(keys.mongoURI);
 
